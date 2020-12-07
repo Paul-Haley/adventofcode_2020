900870a5dac8d142ae2826d627bf13acfa722196
Paul-Haley <paul.haley@bigpond.com>
2020-12-07 19:34:50 +1000

    day 1 part 2

--- 01.py
+++ 01.py
@@ -1,13 +1,26 @@
-n = sorted([int(l.strip()) for l in open("input01.txt").readlines()])
+def find_comp(n, t):
+    a, b = 0, len(n) - 1
+    while a < b:
+        s = n[a] + n[b]
+        if s == t:
+            return a, b
+        if s < t:
+            a += 1
+        if s > t:
+            b -= 1
+    return -1, -1
 
-c = 2020
-a, b = 0, len(n) - 1
-while a < b:
-    s = n[a] + n[b]
-    if s == c:
-        print(n[a]*n[b])
-        exit()
-    if s < c:
-        a += 1
-    if s > c:
-        b -= 1
+
+def find3():
+    n = sorted([int(l.strip()) for l in open("input01.txt").readlines()])
+
+    t = 2020
+    for i in n:
+        a, b = find_comp(n, t - i)
+        if n[a] + n[b] + i == t:
+            print(n[a] * n[b] * i)
+            exit()
+
+
+if __name__ == "__main__":
+    find3()
