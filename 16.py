0def8ff23f63a4ef0e2f6a8059ef1979a79ef4a2
Paul Haley <paul.haley@bigpond.com>
2021-01-05 00:51:24 +1000

    WIP: day 16 part 2

--- 16.py
+++ 16.py
@@ -25,16 +25,29 @@ def read_ticket(ticket_text):
     return tickets
 
 
-def apply_rules_part1(rules, nearby_tickets):
-    scanning_error_rate = 0
+def remove_invalid(rules, nearby_tickets):
     for ticket in nearby_tickets:
         for number in ticket:
             for rule in rules:
                 if rule[1] <= number <= rule[2] or rule[3] <= number <= rule[4]:
                     break
             else:
-                scanning_error_rate += number
-    return scanning_error_rate
+                nearby_tickets.remove(ticket)
+
+
+def identify_possible_columns(rules, nearby_transpose):
+    rules_to_columns = dict()
+    for ci in range(len(nearby_transpose)):
+        for ri in range(len(rules)):
+            rule = rules[ri]
+            for number in nearby_transpose[ci]:
+                if not (rule[1] <= number <= rule[2] or rule[3] <= number <= rule[4]):
+                    break
+            else:
+                rule_columns = rules_to_columns.get(ri, set())
+                rule_columns.add(ci)
+                rules_to_columns[ri] = rule_columns
+    return rules_to_columns
 
 
 if __name__ == '__main__':
@@ -43,4 +56,22 @@ if __name__ == '__main__':
     rules = get_rules(parts[0])
     your_ticket = read_ticket(parts[1])[0]
     nearby_tickets = read_ticket(parts[2])
-    print(apply_rules_part1(rules, nearby_tickets))
+    remove_invalid(rules, nearby_tickets)
+    nearby_transpose = list(map(list, zip(*nearby_tickets)))
+    rules_to_columns = identify_possible_columns(rules, nearby_transpose)
+    # for rule in sorted(rules_to_columns):
+    #     print(rule, rules_to_columns[rule])
+    confirmed_column_mappings = dict()
+    while len(confirmed_column_mappings) != len(rules):
+        identified_column = -1
+        for rule in rules_to_columns:
+            if len(rules_to_columns[rule]) == 1:
+                column = rules_to_columns[rule].pop()
+                confirmed_column_mappings[rule] = column
+                identified_column = column
+                rules_to_columns.pop(rule)
+                break
+        if identified_column != -1:
+            for rule in rules_to_columns:
+                rules_to_columns[rule].discard(identified_column)
+    print(confirmed_column_mappings)
